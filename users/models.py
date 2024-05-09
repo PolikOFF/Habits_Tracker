d202b161420f3db47334886cfb6f649e7cd757df
Andrew_Polik <polik.24@mail.ru>
2024-05-09 23:34:03 +0300

    Прописана модель пользователя.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, max_length=100, verbose_name='электронная почта')
+    telegram_id = models.CharField(max_length=100, verbose_name='id телеграм-чата', **NULLABLE)
+    name = models.CharField(max_length=200, verbose_name='имя пользователя', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        if self.name:
+            return f'Имя - {self.name}, Электронная почта - {self.email}.'
+        return f'Электронная почта - {self.email}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
