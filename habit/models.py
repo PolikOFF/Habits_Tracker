5df8afc5aa8019bfb8a7bc0e151f6ffb950917b8
Andrew_Polik <polik.24@mail.ru>
2024-05-10 00:43:06 +0300

    Частично сделана модель привычки(понять как связывать???)

--- habit/models.py
+++ habit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Habit(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='создатель привычки', **NULLABLE),
+    place = models.CharField(max_length=255, verbose_name='место выполнения привычки'),
+    time = models.TimeField(default='14:00:00', verbose_name='время выполнения привычки'),
+    action = models.CharField(max_length=255, verbose_name='действие'),
+    pleasant_habit = models.BooleanField(verbose_name='признак приятной привычки'),
+    related_habit = models.ForeignKey
