b9dd1f7362abdf5277f9f676e6a3cbac0df27609
Andrew_Polik <polik.24@mail.ru>
2024-06-02 23:02:00 +0300

    Подправлена модель привычки. Запросы работают корректно.

--- habit/models.py
+++ habit/models.py
@@ -3,11 +3,11 @@ from users.models import NULLABLE
 
 
 class Habit(models.Model):
-    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='создатель привычки', **NULLABLE),
-    place = models.CharField(max_length=255, verbose_name='место выполнения привычки', **NULLABLE),
-    time = models.TimeField(default='14:00:00', verbose_name='время выполнения привычки', **NULLABLE),
-    action = models.CharField(max_length=255, verbose_name='действие'),
-    pleasant_habit = models.BooleanField(default=False, verbose_name='признак приятной привычки'),
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='создатель привычки', **NULLABLE)
+    place = models.CharField(max_length=255, verbose_name='место выполнения привычки', **NULLABLE)
+    time = models.TimeField(default='14:00:00', verbose_name='время выполнения привычки', **NULLABLE)
+    action = models.CharField(default='создать действие', max_length=255, verbose_name='действие')
+    pleasant_habit = models.BooleanField(default=False, verbose_name='признак приятной привычки')
     related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='связанная привычка', **NULLABLE)
     periodicity = models.IntegerField(verbose_name='периодичность выполнения(в днях)')
     reward = models.CharField(max_length=150, verbose_name='вознаграждение', **NULLABLE)
