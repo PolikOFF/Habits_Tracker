8cd40574d8f2194588e6b7f6ebe6beecb2ad68c2
Andrew_Polik <polik.24@mail.ru>
2024-06-13 01:28:25 +0300

    Написаны тесты на CRUD привычки.

--- habit/tests.py
+++ habit/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from habit.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            email='test@test.ru',
+            password='test',
+            is_superuser=True,
+            is_staff=True,
+        )
+        # аутентификация клиента
+        self.client.force_authenticate(user=self.user)
+
+        self.habit = Habit.objects.create(
+            action='действие',
+            place='место',
+            periodicity='1',
+            time_to_complete='100'
+        )
+
+    def test_get_list(self):
+        """Тест просмотра списка привычек."""
+
+        response = self.client.get("/habit/list/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create(self):
+        """Тест на создание привычки."""
+        data = {
+            'action': 'действие',
+            'place': 'место',
+            'periodicity': '1',
+            'time_to_complete': '100'
+        }
+
+        response = self.client.post('/habit/create/', data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update(self):
+        """Тест на обновление привычки."""
+        habit = Habit.objects.create(
+            place="test", action="test", periodicity=1, time_to_complete=100, owner=self.user
+        )
+
+        data = {'place': 'test1'}
+
+        response = self.client.patch(f'/habit/update/{habit.id}/', data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        """Тест на удаление привычки."""
+        habit = Habit.objects.create(
+            place="test", action="test", periodicity=1, time_to_complete=100, owner=self.user
+        )
+
+        response = self.client.delete(f'/habit/delete/{habit.id}/')
+
+    def test_retrieve(self):
+        """Тест просмотра одной привычки."""
+        habit = Habit.objects.create(
+            place="test", action="test", periodicity=1, time_to_complete=100, owner=self.user
+        )
+
+        response = self.client.get(f'/habit/get/{habit.id}/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
